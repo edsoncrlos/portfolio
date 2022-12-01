d11a31c58497c04772a39d53368f0804efd3bb3e
Edson Carlos <edsoncarlos.dev@gmail.com>
2022-12-01 08:48:22 -0300

    fix: add auto overflow when esc key is pressed

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -2,12 +2,19 @@
 const portfolioImages = document.querySelectorAll('.portfolio__card > .portfolio__image');
 const modals = document.querySelectorAll('.portfolio__card > .portfolio__modal');
 const buttonsCloseModal = document.querySelectorAll('.portfolio__card .portfolio__close-modal');
+const escKeyCode = 27;
 
 for (let i = 0; i < portfolioImages.length; i++) {
     portfolioImages[i].addEventListener('click', () => {
         document.documentElement.style.overflowY = 'hidden';
         
         modals[i].showModal();
+    });
+
+    modals[i].addEventListener('keydown', (e) => {
+        if (e.keyCode == escKeyCode) {
+            document.documentElement.style.overflowY = 'auto';
+        }        
     });
     
     buttonsCloseModal[i].addEventListener('click', () => {
